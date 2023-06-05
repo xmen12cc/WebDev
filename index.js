6d951e9a14189bf796879d7c507a5c59dd203f98
xmen12cc <79724781+xmen12cc@users.noreply.github.com>
2023-06-05 17:54:45 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+//declaring variables
+let express = require('express');
+let app = express();
+
+app.set('view engine', 'ejs');
+
+//declaring the links and rendering of pages
+app.get('/', (req,res)=>{
+    res.render('pages/home');
+});
+app.get('/about', (req,res)=>{
+    res.render('pages/about');
+});
+app.get('/projects', (req,res)=>{
+    res.render('pages/projects');
+});
+app.get('/contact', (req,res)=>{
+    res.render('pages/contact');
+});
+app.get('/services', (req,res)=>{
+    res.render('pages/services');
+});
+app.get('public/pdf/myresume.pdf', function(req, res) {  
+    res.download('public/pdf/myresume.pdf');
+});
+
+function redirectToHomePage() {
+    window.location.href = "/";
+  }
+
+//declaring the public folder and the init of the page on port 4000
+app.use(express.static('public'));
+app.listen(4000);
